e470eba90e3dad5b41eda426e0cc6b438764a806
wafia7510 <wafiadr2@gmail.com>
2025-06-11 03:33:07 +0500

    Set DEBUG to False and finalized production settings

--- kittyconnect/settings.py
+++ kittyconnect/settings.py
@@ -27,18 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
-
-
-
-
-
-
-
-
-
+DEBUG = env.bool("DEBUG", default=False)
 
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default="127.0.0.1")
